f12866a2059c4245a72aacd05b6f382bb3c70bac
Harveen Singh Chadha <harveen.chadha@Harveens-MacBook-Pro.local>
2020-05-14 17:33:51 +0530

    Harveen | Adding wav conversion

--- src/download.py
+++ src/download.py
@@ -2,28 +2,59 @@ from pytube import Playlist
 import pytube
 import os
 from tqdm import tqdm
+import subprocess
+import glob
 
 class DownloadVideo(object):
     def __init__(self):
-        pass
+        self.audio_path = []
 
     def download_playlist(self, playlist_url, output_path):
         playlist= Playlist(playlist_url)
+        print(playlist)
+
+        videos_downloaded = []
         
-        for i,link in tqdm(enumerate(playlist)):
+        for i, link in tqdm(enumerate(playlist)):
             print("Downloading Video: ", i)
-            self.download_video(link, output_path, i)
+            audio_path = self.download_video(video_url = link, output_path = output_path, filename = i)
+
+            videos_downloaded.append(audio_path)
+
+        self.audio_path = videos_downloaded
 
     def download_video(self, video_url, output_path, filename):
         try:
             yt = pytube.YouTube(video_url)
+            yt.streams.filter(only_audio=True)[0].download(output_path = output_path, filename = str(filename))
+            return output_path + './' + filename
         except:
             print("Connection error at video ", video_url)
 
-        yt.streams.filter(only_audio=True)[0].download(output_path = output_path, filename=filename)
+        #print("filename is ", filename)
+
+
+    def convert_to_wav(self, list_paths = None):
+        if list_paths is not None:
+            self.audio_path = list_paths
+
+        for file in self.audio_path:
+
+            input_file_name = file
+            output_file_name = file.split('/')[-1].split('.')[0] + '.wav'
+            output_file_path = file.split('/')[:-1] + '/' + output_file_name
+
+            command = f'ffmpeg -i {input_file_name} -ar 44100 -ac 1 -bits_per_raw_sample 16 {output_file_name}'
+            subprocess.call(command, shell=True)
+        
+
+        
 
 
 
 if __name__ == "__main__":
     dwn = DownloadVideo()
-    dwn.download_video('https://www.youtube.com/playlist?list=PL1D6nWQpbEZdtH4G1TZNrBVj1zBfTOhA7', './', '1')
+    #dwn.download_playlist('https://www.youtube.com/playlist?list=PL1D6nWQpbEZdtH4G1TZNrBVj1zBfTOhA7', './')
+    list_dir = glob.glob('/home/harveen.chadha/gcs_mnt/data/audiotospeech/raw/landing/hindi/audio/joshtalks/*.mp4')
+
+    dwn.convert_to_wav(list_dir)
