5c427df65ce381765fe3847c5580737af7bd8cd6
agupta54 <anirudhgupta12@gmail.com>
2020-05-21 07:38:43 +0530

    Anirudh | Added test for fit function in SNR

--- src/tests/test_snr.py
+++ src/tests/test_snr.py
@@ -11,3 +11,13 @@ class TestSNR(unittest.TestCase):
     def test_compute_file_snr(self):
         obj = SNR()
         self.assertEqual(obj.compute_file_snr('test_resources/test.wav'), 19.939861)
+
+    def test_fit_return_type(self):
+        obj = SNR()
+        file_snr = obj.fit('test_resources/')
+        self.assertEqual(type(file_snr), type({}))
+
+    def test_fit_return_value(self):
+        obj = SNR()
+        file_snr = obj.fit('test_resources/')
+        self.assertEqual(file_snr['test_resources/test.wav'], 19.939861)
