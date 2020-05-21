b3719ea62d33013375fbad6f5a63cc49a53489f2
agupta54 <anirudhgupta12@gmail.com>
2020-05-21 07:01:55 +0530

    Anirudh | Fixing SNR script

--- src/scripts/download.py
+++ src/scripts/download.py
@@ -1,6 +1,5 @@
 from pytube import Playlist
 import pytube
-import os
 from tqdm import tqdm
 import subprocess
 import glob
@@ -8,20 +7,22 @@ from datetime import datetime
 
 import uuid
 
+
 class DownloadVideo(object):
     def __init__(self, extension='mp4'):
         self.audio_path = []
         self.extension = extension
 
     def download_playlist(self, playlist_url, output_path, filename_prefix):
-        playlist= Playlist(playlist_url)
+        playlist = Playlist(playlist_url)
 
         videos_downloaded = []
-        
+
         for index, link in tqdm(enumerate(playlist)):
             print("Downloading Video: ", index)
 
-            audio_path = self.download_video(video_url = link, output_path = output_path, filename_prefix = filename_prefix + '_pl_'+str(index) )
+            audio_path = self.download_video(video_url=link, output_path=output_path,
+                                             filename_prefix=filename_prefix + '_pl_' + str(index))
             print(audio_path)
             videos_downloaded.append(audio_path)
 
@@ -29,14 +30,13 @@ class DownloadVideo(object):
         return videos_downloaded
 
     def download_video(self, video_url, output_path, filename_prefix):
-        
+
         name = self.generate_unique_filename(filename_prefix)
         filename = name + '.' + self.extension
 
-
         try:
             yt = pytube.YouTube(video_url)
-            yt.streams.filter(only_audio=True)[0].download(output_path = output_path, filename = str(name))
+            yt.streams.filter(only_audio=True)[0].download(output_path=output_path, filename=str(name))
             return output_path + '/' + filename
         except:
             print("Connection error at video ", video_url)
@@ -47,66 +47,60 @@ class DownloadVideo(object):
 
         if filename_prefix is None:
             file_name_for_saving = str(uuid.uuid4())
-        else: 
+        else:
             file_name_for_saving = filename_prefix
-        
+
         return date_part + '_' + file_name_for_saving
 
-
-
-    def convert_to_wav(self, output_dir = None, list_paths = None):
+    def convert_to_wav(self, output_dir=None, list_paths=None):
         if list_paths is not None:
             self.audio_path = list_paths
 
         output_file_paths = []
         for file in self.audio_path:
             print(file)
-            
+
             input_file_name = file
-            
+
             output_file_name = file.split('/')[-1].split('.')[0] + '.wav'
-            
+
             if output_dir is None:
                 output_file_path = "/".join(file.split('/')[:-1]) + '/' + output_file_name
             else:
                 output_file_path = output_dir + '/' + output_file_name
 
-
             command = f"ffmpeg -i {input_file_name} -ar 16000 -ac 1 -bits_per_raw_sample 16 {output_file_path}"
             subprocess.call(command, shell=True)
 
             output_file_paths.append(output_file_path)
-        
+
         return output_file_paths
 
-        
-
-    def fit(self, link,  output_dir_path, mode='video',filename_prefix = None, convert_to_wav = True, output_wav_dir = None):
+    def fit(self, link, output_dir_path, mode='video', filename_prefix=None, convert_to_wav=True, output_wav_dir=None):
         ## modes present video, playlist, videolist
 
         if mode == 'video':
             path = self.download_video(link, output_dir_path, filename_prefix)
 
             print('path received is ', path)
-            
-            if convert_to_wav:
-                return self.convert_to_wav(output_dir= output_wav_dir, list_paths=[path])
-                
-            return path
 
+            if convert_to_wav:
+                return self.convert_to_wav(output_dir=output_wav_dir, list_paths=[path])
+
+            return path
 
         if mode == 'playlist':
             list_paths = self.download_playlist(link, output_dir_path, filename_prefix)
 
             if convert_to_wav:
-                return self.convert_to_wav(output_dir= output_wav_dir, list_paths=list_paths)
+                return self.convert_to_wav(output_dir=output_wav_dir, list_paths=list_paths)
             return list_paths
 
         if mode == 'videolist':
             list_paths = []
 
             for index, link in enumerate(videolist):
-                
+
                 path = ''
                 if filename_prefix is not None:
                     new_prefix = filename_prefix + '_' + strindex
@@ -115,23 +109,18 @@ class DownloadVideo(object):
                 path = self.download_video(link, output_dir_path, filename_prefix)
                 list_paths.append(path)
 
-
-            
             if convert_to_wav:
-                return self.convert_to_wav(output_dir= output_wav_dir, list_paths=list_paths)
+                return self.convert_to_wav(output_dir=output_wav_dir, list_paths=list_paths)
 
             return list_paths
         print('Finished Downloading')
 
 
-
-
-
 if __name__ == "__main__":
     dwn = DownloadVideo()
-    #dwn.download_playlist('https://www.youtube.com/playlist?list=PL1D6nWQpbEZdtH4G1TZNrBVj1zBfTOhA7', './')
+    # dwn.download_playlist('https://www.youtube.com/playlist?list=PL1D6nWQpbEZdtH4G1TZNrBVj1zBfTOhA7', './')
     dwn.download_video('https://www.youtube.com/watch?v=TMoCrV3D1kU&feature=youtu.be', './data_demo', '1')
 
     # list_dir = glob.glob(r'..//data/*.mp4')
     # print(list_dir)
-    dwn.convert_to_wav(list_paths = ['./data_demo/1.mp4'])
+    dwn.convert_to_wav(list_paths=['./data_demo/1.mp4'])
--- src/scripts/snr.py
+++ src/scripts/snr.py
@@ -1,39 +1,26 @@
 import glob
 import os, sys
-import glob
 import subprocess
-import tempfile
 import soundfile as sf
-import numpy as np
 from tqdm import tqdm
-from multiprocessing import Pool
+
 
 class SNR(object):
 
     def compute_file_snr(self, file_path, CURRENT_PATH):
         """ Convert given file to required format with FFMPEG and process with WADA."""
         _, sr = sf.read(file_path)
-        # new_file = file_path.replace(".wav", "_tmp.wav")
-        # if sr != 16000:
-        #     command = f'ffmpeg -i "{file_path}" -ac 1 -acodec pcm_s16le -y -ar 16000 "{new_file}"'
-        # else:
-        #     command = f'cp "{file_path}" "{new_file}"'
-        # os.system(command)
-        #print('here')
-        #command = [f'"{CURRENT_PATH}/WadaSNR/Exe/WADASNR"', f'-i "{new_file}"', f'-t "{CURRENT_PATH}/WadaSNR/Exe/Alpha0.400000.txt"', '-ifmt mswav']
         command = f'"{CURRENT_PATH}/WadaSNR/Exe/WADASNR" -i "{file_path}" -t "{CURRENT_PATH}/WadaSNR/Exe/Alpha0.400000.txt" -ifmt mswav'
         output = subprocess.check_output(command, shell=True)
         try:
             output = float(output.split()[-3].decode("utf-8"))
         except:
             raise RuntimeError(" ".join(command))
-        #os.system(f'rm "{new_file}"')
-        print(output)
         return output
 
-
     def fit(self, input_file_dir):
-        wav_files = input_file_dir
+
+        wav_files = glob.glob(f"{input_file_dir}*wav")
         file_snrs = {}
 
         CURRENT_PATH = '../..'
@@ -45,10 +32,8 @@ class SNR(object):
             print(f"File {key} has an snr value of {value}")
         return file_snrs
 
-    def fit_and_move(self, input_file_dir, threshold, output_file_dir ):
+    def fit_and_move(self, input_file_dir, threshold, output_file_dir):
         local_dict = self.fit(input_file_dir)
-
-        print(local_dict)
 
         # if output_file_dir is None:
         #     output_file_dir = input_file_dir
@@ -61,7 +46,6 @@ class SNR(object):
 
         if not os.path.exists(rejected_dir):
             os.mkdir(rejected_dir)
-        
 
         for key, value in local_dict.items():
             audio_file_name = key.split('/')[-1]
@@ -69,7 +53,7 @@ class SNR(object):
 
             if value >= threshold:
                 ## copy to clean directory of output
-                clean_dir_local = clean_dir + '/' + audio_file_name 
+                clean_dir_local = clean_dir + '/' + audio_file_name
                 print(clean_dir_local)
                 command = f'mv "{key}" "{clean_dir_local}"'
             else:
@@ -79,11 +63,12 @@ class SNR(object):
                 command = f'mv "{key}" "{rejected_dir_local}"'
 
             os.system(command)
-            output = subprocess.check_output(command, shell=True)
+            #output = subprocess.check_output(command, shell=True)
 
 
 if __name__ == "__main__":
     snr_obj = SNR()
-    input_file_dir = '/home/harveen.chadha/gcmount/data/audiotospeech/raw/landing/hindi/audio/testing/cutaudio'
-    threshold = 20
-    snr_obj.fit_and_move(input_file_dir , threshold)
+    input_file_dir = '/home/anirudh/Projects/AudioSpeech/vad/'
+    # input_file_dir = '/home/harveen.chadha/gcmount/data/audiotospeech/raw/landing/hindi/audio/testing/cutaudio'
+    threshold = 16
+    snr_obj.fit_and_move(input_file_dir, threshold, '/home/anirudh/Projects/AudioSpeech/vad/output')
