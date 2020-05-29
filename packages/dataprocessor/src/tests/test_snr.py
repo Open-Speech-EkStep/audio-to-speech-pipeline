cbe44cfc471e56fc2c82bcf311f4fdf070ea0873
INE12367418 <souyo.sen@tesco.com>
2020-05-29 15:04:12 +0530

    Soujyo | Fix input paths for existing SNR test cases

--- packages/dataprocessor/src/tests/test_snr.py
+++ packages/dataprocessor/src/tests/test_snr.py
@@ -11,19 +11,19 @@ class TestSNR(unittest.TestCase):
 
     def test_compute_file_snr(self):
         obj = SNR()
-        self.assertEqual(obj.compute_file_snr('src/tests/test_resources/test.wav'), 19.939861)
+        self.assertEqual(obj.compute_file_snr('src/tests/test_resources/input/test.wav'), 19.939861)
 
     def test_fit_return_type(self):
         obj = SNR()
-        input_dir = 'src/tests/test_resources/'
+        input_dir = 'src/tests/test_resources/input/'
         wav_files = glob.glob(f"{input_dir}*wav")
         file_snr = obj.fit(wav_files)
         self.assertEqual(type(file_snr), type({}))
 
     def test_fit_return_value(self):
         obj = SNR()
-        input_dir = 'src/tests/test_resources/'
+        input_dir = 'src/tests/test_resources/input/'
         wav_files = glob.glob(f"{input_dir}*wav")
         file_snr = obj.fit(wav_files)
-        self.assertEqual(file_snr['src/tests/test_resources/test.wav'], 19.939861)
-        self.assertEqual(file_snr['src/tests/test_resources/test.wav'], 19.939861)
+        self.assertEqual(file_snr['src/tests/test_resources/input/test.wav'], 19.939861)
+        self.assertEqual(file_snr['src/tests/test_resources/input/test.wav'], 19.939861)
