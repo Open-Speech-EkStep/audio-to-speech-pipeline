eb77ccac8986c7bf5a226937d1c963c4de9a7159
ggupta36 <gauravgupta_itm@rediffmail.com>
2020-06-02 10:54:22 +0530

    Gaurav | Added column name check while uploading metadata to cloud sql

--- packages/dataprocessor/src/scripts/metadata_uploader.py
+++ packages/dataprocessor/src/scripts/metadata_uploader.py
@@ -12,7 +12,7 @@ class CloudSQLUploader():
         with open(file_path, 'r') as f:
             conn = db_conn.raw_connection()
             cursor = conn.cursor()
-            cmd = 'COPY media_metadata_staging FROM STDIN WITH (FORMAT CSV, HEADER)'
+            cmd = 'COPY media_metadata_staging(raw_file_name,duration,title,speaker_name,audio_id,cleaned_duration,num_of_speakers,language,has_other_audio_signature,type,source,experiment_use,utterances_files_list,source_url,speaker_gender,source_website,experiment_name,mother_tongue,age_group,recorded_state,recorded_district,recorded_place,recorded_date,purpose) FROM STDIN WITH (FORMAT CSV, HEADER)'
             cursor.copy_expert(cmd, f)
             conn.commit()
 
