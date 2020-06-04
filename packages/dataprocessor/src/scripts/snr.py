5623d12b07888037718d26e2ea7007ca20fe53fd
ggupta36 <gauravgupta_itm@rediffmail.com>
2020-06-04 14:38:27 +0530

    Gaurav | Rounded off cleaned duration to 2 decimal places

--- packages/dataprocessor/src/scripts/snr.py
+++ packages/dataprocessor/src/scripts/snr.py
@@ -85,7 +85,7 @@ class SNR(object):
                 print(command)
                 print(command_text)
 
-            metadata["cleaned_duration"] = math.floor(sum(clean_audio_duration) / 60)
+            metadata["cleaned_duration"] = round((sum(clean_audio_duration) / 60),2)
             metadata["audio_id"] = audio_id
             metadata['utterances_files_list'] = str(list_file_utterances_with_duration)
             metadata.to_csv(metadata_file_name,index=False)
