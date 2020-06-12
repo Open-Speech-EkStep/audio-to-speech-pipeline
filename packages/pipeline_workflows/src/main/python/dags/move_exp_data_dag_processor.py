efd726b0929b77d1f97aa90001eee997e95576a7
soujyo <soujyo.sen@thoughtworks.com>
2020-06-12 16:42:49 +0530

    Soujyo | Fix dag code bugs

--- packages/pipeline_workflows/src/main/python/dags/move_exp_data_dag_processor.py
+++ packages/pipeline_workflows/src/main/python/dags/move_exp_data_dag_processor.py
@@ -16,13 +16,11 @@ class mydict(dict):
 
 def get_variables():
     global source_chunk_path
-    global destination_utterances_path
     global bucket_name
     global archive_utterances_path
     global integration_processed_path
     global experiment_output
     source_chunk_path = Variable.get("utteranceschunkpath")
-    destination_utterances_path = Variable.get("destinationutterancespath")
     archive_utterances_path = Variable.get("archiveutterancespath")
     integration_processed_path = Variable.get("integrationprocessedpath")
     experiment_output = Variable.get("experimentoutput")
@@ -43,11 +41,12 @@ def count_utterances_file_chunks(**kwargs):
     Variable.set("utteranceschunkslist", utterances_names)
 
 
-def move_utterance_chunk(bucket_name, source_file_name, local_file_name):
+def move_utterance_chunk(bucket_name, source_file_name, experiment_name):
     get_variables()
-    archive_utterances_file_name = archive_utterances_path + source_file_name.split('/')[-1]
+    archive_utterances_file_name = archive_utterances_path + experiment_name + "/" + source_file_name.split('/')[
+        -1]
     move_blob(bucket_name, source_file_name, bucket_name, archive_utterances_file_name)
-    os.remove(local_file_name)
+    # os.remove(local_file_name)
 
 
 def copy_utterances(src_file_name, **kwargs):
@@ -65,7 +64,7 @@ def copy_utterances(src_file_name, **kwargs):
             utterance_file_name = str(file_name).split(".")[0]
             print(file_name, utterance_file_name, audio_id)
             for extn in extn_list:
-                source_blob_name = integration_processed_path + source + "/" + str(
+                source_blob_name = integration_processed_path + source.lower() + "/" + str(
                     audio_id) + "/clean/" + utterance_file_name + "." + extn
                 destination_blob_name = experiment_output + experiment_name + "/" + str(
                     audio_id) + "/" + utterance_file_name + "." + extn
@@ -75,12 +74,11 @@ def copy_utterances(src_file_name, **kwargs):
                     destination_bucket_name=bucket_name,
                     destination_blob_name=destination_blob_name,
                 )
-            move_utterance_chunk(bucket_name, src_file_name, local_file_name)
+        move_utterance_chunk(bucket_name, src_file_name, experiment_name)
 
     finally:
-        print("done")
+        print("All files have been copied")
 
-        print("******src_file_name******", src_file_name)
 
 if __name__ == "__main__":
     pass
