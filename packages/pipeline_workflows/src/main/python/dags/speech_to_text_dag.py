e543f4833c3608b795f051a432500a61eade5c27
Heera ballabh <hballabh@onecallcm.com>
2020-06-26 16:34:15 +0530

    Add datacataloguer after pipeline..

--- packages/pipeline_workflows/src/main/python/dags/speech_to_text_dag.py
+++ packages/pipeline_workflows/src/main/python/dags/speech_to_text_dag.py
@@ -51,6 +51,16 @@ def create_dag(dag_id,
 
         audio_file_ids = json.loads(Variable.get("audiofileids"))
 
+        data_prep_cataloguer = kubernetes_pod_operator.KubernetesPodOperator(
+            task_id='data-prep-cataloguer',
+            name='data-prep-cataloguer',
+            cmds=["python", "-m", "src.scripts.db_normalizer", "cluster", "ekstepspeechrecognition-dev", "data/audiotospeech/config/datacataloguer-prep/config.yaml"],
+            namespace='composer-1-10-4-airflow-1-10-6-3b791e93',
+            startup_timeout_seconds=300,
+            secrets=[secret_file],
+            image='us.gcr.io/ekstepspeechrecognition/data_prep_cataloguer:1.0.0',
+            image_pull_policy='Always')
+
         for audio_file_id in audio_file_ids[dag_id]:
             # input_path = tobe_processed_path + dag_id + '/' + str(audio_file_id)
             data_prep_task = kubernetes_pod_operator.KubernetesPodOperator(
@@ -58,10 +68,10 @@ def create_dag(dag_id,
                 name='data-prep-stt',
                 cmds=["python","-m" ,"src.scripts.pipeline", "cluster", bucket_name, stt_config_path, dag_id,
                       audio_file_id, source_audio_format[dag_id]],
-                namespace='composer-1-10-4-airflow-1-10-6-6928624a',
+                namespace='composer-1-10-4-airflow-1-10-6-3b791e93',
                 startup_timeout_seconds=300,
                 secrets=[secret_file],
-                image='us.gcr.io/ekstepspeechrecognition/dataprocessor:1.0.2',
+                image='us.gcr.io/ekstepspeechrecognition/dataprocessor:1.0.0',
                 image_pull_policy='Always')
 
             move_to_processed = PythonOperator(
@@ -70,7 +80,7 @@ def create_dag(dag_id,
                 op_kwargs={'source': dag_id, 'audio_id': audio_file_id},
                 dag_number=dag_number)
 
-            fetch_audio_ids >> data_prep_task >> move_to_processed
+            fetch_audio_ids >> data_prep_task >> move_to_processed >> data_prep_cataloguer
 
     return dag
 
