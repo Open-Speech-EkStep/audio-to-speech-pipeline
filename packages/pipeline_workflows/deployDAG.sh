21bbfc01339dfd2146efbf4e8321f63a1590a463
Heera ballabh <hballabh@onecallcm.com>
2020-07-08 17:46:57 +0530

    Set env variable for airflow while deploy

--- packages/pipeline_workflows/deployDAG.sh
+++ packages/pipeline_workflows/deployDAG.sh
@@ -17,6 +17,7 @@ done
 variables_json="./src/main/python/resources/airflow_config_file.json"
 echo "$variables_json file"
 echo "hello "
+gcloud components install kubectl
 
 gcloud composer environments storage data import --environment $COMPOSER_ENV --location $LOCATION --source ./src/main/python/resources/airflow_config_file.json
 gcloud composer environments run $COMPOSER_ENV --location $LOCATION variables -- --import /home/airflow/gcs/data/airflow_config_file.json
