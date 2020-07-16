f49fc83b5a993812972b5b438c89e3124d365d58
Rajat Singhal <rajasinghal@gmail.com>
2020-07-16 18:45:55 +0200

    171| Rajat| fix test_merge_chunks args

--- packages/dataprocessor/src/tests/test_merge_chunks.py
+++ packages/dataprocessor/src/tests/test_merge_chunks.py
@@ -6,9 +6,8 @@ from src.scripts.merge_chunks import merge_chunks
 class MergeChunks(unittest.TestCase):
     def test_merge_chunks(self):
         chunks_dir = './src/tests/test_resources/input/chunks'
-        out_dir = './src/tests/test_resources/output/merged'
         separator_file_path = './src/tests/test_resources/input/chunks' + '/separator/hoppi.wav'
-        merged_file_path = merge_chunks(chunks_dir, out_dir, separator_file_path, 'chunk')
+        merged_file_path = merge_chunks(chunks_dir, separator_file_path, 'chunk', 'merged.wav')
         self.assertEquals('merged.wav', merged_file_path)
 
 if __name__ == '__main__':
