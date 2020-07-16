6640c1e7d775ab3156bf8a34aecfefecd66999c4
Rajat Singhal <rajasinghal@gmail.com>
2020-07-16 20:33:09 +0200

    171| Rajat| use mkdirs to create intermediate dirs

--- packages/dataprocessor/src/scripts/snr.py
+++ packages/dataprocessor/src/scripts/snr.py
@@ -49,10 +49,10 @@ class SNR(object):
         list_file_utterances_with_duration=[]
 
         if not os.path.exists(clean_dir):
-            os.mkdir(clean_dir)
+            os.mkdirs(clean_dir)
 
         if not os.path.exists(rejected_dir):
-            os.mkdir(rejected_dir)
+            os.mkdirs(rejected_dir)
 
         for key, value in local_dict.items():
             audio_file_name = key.split('/')[-1]
