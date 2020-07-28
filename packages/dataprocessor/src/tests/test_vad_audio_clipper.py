a398479fcb35844077ead9cba006ac06e76ef0f1
Rajat Singhal <rajasinghal@gmail.com>
2020-07-28 16:50:12 +0530

    171|Rajat, Avneesh| refactor tests

--- packages/dataprocessor/src/tests/test_vad_audio_clipper.py
+++ packages/dataprocessor/src/tests/test_vad_audio_clipper.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import unittest
 
 from src.scripts.vad_audio_clipper import create_audio_clips
@@ -7,28 +8,23 @@ from src.scripts.vad_audio_clipper import create_audio_clips
 class VadAudioClipper(unittest.TestCase):
     def test_vad_clip(self):
         aggressiveness = 2
-        output_dir = './src/tests/test_resources/output'
+        output_dir = './src/tests/test_resources/output/chunks'
         input_dir = './src/tests/test_resources/input'
         file_name = 'test.wav'
         input_file_path = input_dir + '/' + file_name
         vad_output_file_path = input_dir + '/' + file_name.replace('.wav', '_vad_output.txt')
         base_chunk_name = 'test.wav'
+        os.makedirs(output_dir)
         create_audio_clips(aggressiveness, input_file_path, output_dir, vad_output_file_path, base_chunk_name)
         actual_chunk_names = os.listdir(output_dir)  # dir is your directory path
-        self.assertEqual(37, len(actual_chunk_names))
-
-        expected_chunk_names = [f'{i}_test.wav' for i in range(37)]
+        expected_chunk_names = [f'{i}_test.wav' for i in range(len(actual_chunk_names))]
         print('list:' + str(expected_chunk_names))
-        self.assertEqual(set(actual_chunk_names), set(expected_chunk_names))
         self.delete_files(output_dir)
+        self.assertEqual(set(actual_chunk_names), set(expected_chunk_names))
 
     def delete_files(self, output_dir):
-        files = os.listdir(output_dir)  # dir is your directory path
-        for f in files:
-            try:
-                os.remove(output_dir + '/' + f)
-            except OSError as e:
-                print("Error: %s : %s" % (f, e.strerror))
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
 
 
 if __name__ == '__main__':
