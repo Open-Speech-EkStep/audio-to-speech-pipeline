cc6869b9733ae88e99e28f8d03846de350daba18
Heera ballabh <hballabh@onecallcm.com>
2020-08-24 15:34:31 +0530

    |Avneesh + Heera| fix db connection.

--- packages/ekstep_data_pipelines/audio_processing/audio_processer.py
+++ packages/ekstep_data_pipelines/audio_processing/audio_processer.py
@@ -149,10 +149,10 @@ class AudioProcessor:
         Uploading the meta data file from local to
         """
 
-        db_conn = self.data_processor.connection
+        db = self.data_processor.db
 
         with open(meta_data_path, 'r') as f:
-            conn = db_conn.raw_connection()
+            conn = db.raw_connection()
             cursor = conn.cursor()
             cmd = 'COPY media_metadata_staging(raw_file_name,duration,title,speaker_name,audio_id,cleaned_duration,num_of_speakers,language,has_other_audio_signature,type,source,experiment_use,utterances_files_list,source_url,speaker_gender,source_website,experiment_name,mother_tongue,age_group,recorded_state,recorded_district,recorded_place,recorded_date,purpose) FROM STDIN WITH (FORMAT CSV, HEADER)'
             cursor.copy_expert(cmd, f)
