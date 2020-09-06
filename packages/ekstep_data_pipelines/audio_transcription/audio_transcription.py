5d7899b3e9d7ad239706490f27b2da51c76ef6a3
Rajat Singhal <rajasinghal@gmail.com>
2020-09-06 19:26:12 +0530

    fix(audio_transcription): fix logger in audio_transcription.py

--- packages/ekstep_data_pipelines/audio_transcription/audio_transcription.py
+++ packages/ekstep_data_pipelines/audio_transcription/audio_transcription.py
@@ -89,6 +89,7 @@ class AudioTranscription:
             f.write(transcription)
 
     def generate_transcription_for_all_utterenaces(self, all_path, language, transcription_client, utterances):
+        LOGGER.info("***generate_transcription_for_all_utterenaces**")
         for file_path in all_path:
             utterance_metadata = self.catalogue_dao.find_utterance_by_name(utterances, file_path.name)
             if utterance_metadata is None:
@@ -97,7 +98,7 @@ class AudioTranscription:
             if utterance_metadata['status'] == 'Rejected':
                 LOGGER.info('Skipping rejected file_name: ' + file_path)
                 continue
-            LOGGER.info('Generating transcription for utterance:' + utterance_metadata)
+            LOGGER.info('Generating transcription for utterance:' + str(utterance_metadata))
             local_clean_path = f"/tmp/clean/{file_path.name}"
             local_rejected_path = f"/tmp/rejected/{file_path.name}"
 
