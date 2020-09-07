ab5cef872e8961f40ed8d2a6a20037e50c38dd8a
Heera ballabh <hballabh@onecallcm.com>
2020-09-07 17:46:41 +0530

    |Avneesh + Heera + Amulya| Fix json issue in snr utils.

--- packages/ekstep_data_pipelines/common/audio_commons/snr_util.py
+++ packages/ekstep_data_pipelines/common/audio_commons/snr_util.py
@@ -107,11 +107,11 @@ class SNR:
 
             metadata["cleaned_duration"] = round((sum(clean_audio_duration) / 60), 2)
             metadata["audio_id"] = audio_id
-            metadata['utterances_files_list'] = str(list_file_utterances_with_duration)
+            metadata['utterances_files_list'] = json.dumps(list_file_utterances_with_duration)
 
             clip_duration = calculate_duration(file_path)
-            
             if snr_value < threshold:
+
                 self.move_file_locally(file_path,  f'{rejected_dir_path}/{audio_file_name}')
                 metadata.to_csv(metadata_file_name, index=False)
                 list_file_utterances_with_duration.append(
