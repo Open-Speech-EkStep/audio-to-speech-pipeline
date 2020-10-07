5330e98f18caf06335c46315f24f3b9c13b16f33
Rajat Singhal <rajasinghal@gmail.com>
2020-10-07 21:50:22 +0530

    chore(postgres): move adaptors registration to postgres_db_client.py

--- packages/ekstep_data_pipelines/common/postgres_db_client.py
+++ packages/ekstep_data_pipelines/common/postgres_db_client.py
@@ -1,7 +1,36 @@
 import psycopg2
 import yaml
 from sqlalchemy import create_engine, select, MetaData, Table, text
+import numpy as np
+from psycopg2._json import Json
+from psycopg2.extensions import register_adapter, AsIs
 
+def addapt_numpy_float64(numpy_float64):
+    return AsIs(numpy_float64)
+
+
+def addapt_numpy_int64(numpy_int64):
+    return AsIs(numpy_int64)
+
+
+def addapt_numpy_float32(numpy_float32):
+    return AsIs(numpy_float32)
+
+
+def addapt_numpy_int32(numpy_int32):
+    return AsIs(numpy_int32)
+
+
+def addapt_numpy_array(numpy_array):
+    return AsIs(tuple(numpy_array))
+
+
+register_adapter(np.float64, addapt_numpy_float64)
+register_adapter(np.int64, addapt_numpy_int64)
+register_adapter(np.float32, addapt_numpy_float32)
+register_adapter(np.int32, addapt_numpy_int32)
+register_adapter(np.ndarray, addapt_numpy_array)
+register_adapter(dict, Json)
 
 class PostgresClient:
     """
