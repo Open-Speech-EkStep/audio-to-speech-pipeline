9ed4fb890ac5a07dedbd631fd9eb059dbcc25741
Rajat Singhal <rajasinghal@gmail.com>
2020-10-12 09:42:38 +0530

    feat(language_identification): save null of language_confidence_score toggle is off

--- packages/data_prep_cataloguer/src/scripts/db_normalizer.py
+++ packages/data_prep_cataloguer/src/scripts/db_normalizer.py
@@ -71,7 +71,7 @@ class Db_normalizer():
         durtion = utterance['duration']
         status = utterance['status']
         fail_reason = utterance.get('reason', '')
-        language_confidence_score = json.dumps(utterance.get('language_confidence_score', {}))
+        language_confidence_score = json.dumps(utterance.get('language_confidence_score', None))
         try:
             if str(utterance.get('snr_value')) == 'nan':
                 snr_value = 0.0
