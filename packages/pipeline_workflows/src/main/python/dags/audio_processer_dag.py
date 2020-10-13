f6989c63830974cfb78c76d38c805f04c3f49cd8
Rajat Singhal <rajasinghal@gmail.com>
2020-10-13 19:25:48 +0530

    feat(resources): provide resource limits from config

--- packages/pipeline_workflows/src/main/python/dags/audio_processer_dag.py
+++ packages/pipeline_workflows/src/main/python/dags/audio_processer_dag.py
@@ -18,7 +18,7 @@ bucket_name = Variable.get("bucket")
 # processed_path = Variable.get("rawprocessedpath")
 env_name = Variable.get("env")
 composer_namespace = Variable.get("composer_namespace")
-# resource_limits = Variable.get("snr_resource_limits")
+resource_limits = Variable.get("snr_resource_limits")
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
 
@@ -72,12 +72,12 @@ def create_dag(dag_id,
                 name='data-normalizer',
                 cmds=["python", "-m", "src.scripts.db_normalizer", "cluster", bucket_name,
                       "data/audiotospeech/config/datacataloguer-prep/config.yaml"],
-                # namespace='composer-1-10-4-airflow-1-10-6-3b791e93',
                 namespace=composer_namespace,
                 startup_timeout_seconds=300,
                 secrets=[secret_file],
                 image=f'us.gcr.io/ekstepspeechrecognition/data_prep_cataloguer:{env_name}_1.0.0',
-                image_pull_policy='Always')
+                image_pull_policy='Always',
+                resources=resource_limits)
         else:
             batches = []
 
@@ -93,7 +93,8 @@ def create_dag(dag_id,
                 startup_timeout_seconds=300,
                 secrets=[secret_file],
                 image=f'us.gcr.io/ekstepspeechrecognition/ekstep_data_pipelines:{env_name}_1.0.0',
-                image_pull_policy='Always')
+                image_pull_policy='Always',
+                resources=resource_limits)
 
             get_file_path_from_gcp_bucket >> data_prep_task >> data_prep_cataloguer
 
