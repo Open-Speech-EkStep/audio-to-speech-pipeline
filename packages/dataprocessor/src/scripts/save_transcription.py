caa474200d380d69572af06f862e4f51393fdcde
soujyo <soujyo.sen@thoughtworks.com>
2020-10-23 12:53:01 +0530

    feat(pipeline):TECH DEBT | Soujyo | Cleanup of all legacy packages from project

--- packages/dataprocessor/src/scripts/save_transcription.py
+++
@@ -1,9 +0,0 @@
-
-def save_transcription(transcription, output_file_path):
-    with open(output_file_path, "w") as f:
-        f.write(transcription)
-
-def save_transcriptions(output_dir, transcriptions, file_name):
-    for index, transcription in enumerate(transcriptions):
-        transcription_file = '{0}/{1}-{2}.txt'.format(output_dir, file_name, index)
-        save_transcription(transcription, transcription_file)
