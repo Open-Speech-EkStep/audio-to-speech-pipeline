6ed3e8698698644a992c4058e7340363885215ea
Rajat Singhal <rajasinghal@gmail.com>
2020-11-04 21:18:28 +0530

    feat(speaker_clustering): update was_noise flag

--- packages/ekstep_data_pipelines/audio_analysis/speaker_analysis/file_cluster_mapping.py
+++ packages/ekstep_data_pipelines/audio_analysis/speaker_analysis/file_cluster_mapping.py
@@ -16,8 +16,8 @@ def file_to_speaker_map(speaker_to_file_map):
     return file_to_speaker
 
 def speaker_to_file_name_map(speaker_to_file_map):
-    speaker_to_file_name = {}
+    speaker_to_utterances = {}
     for speaker in speaker_to_file_map:
-        filenames = list(map(lambda f: f.split('/')[-1], speaker_to_file_map.get(speaker)))
-        speaker_to_file_name[speaker] = filenames
-    return speaker_to_file_name
+        utterances = list(map(lambda f: (f[0].split('/')[-1], f[1]), speaker_to_file_map.get(speaker)))
+        speaker_to_utterances[speaker] = utterances
+    return speaker_to_utterances
--- packages/ekstep_data_pipelines/ekstep_pipelines_tests/audio_analysis_test/speaker_analysis/file_cluster_mapping_tests.py
+++ packages/ekstep_data_pipelines/ekstep_pipelines_tests/audio_analysis_test/speaker_analysis/file_cluster_mapping_tests.py
@@ -43,28 +43,28 @@ class FileClusterMappingTests(unittest.TestCase):
 
     def test_should_create_speaker_to_filename_map(self):
         file_map_dict = {'mkb_sp_0':
-                             ['ekstep_pipelines_tests/resources/CEC/202008120411310459/clean/13_Hashiye_Ka_Siddhant_aur_Sahitya.wav',
-                              'ekstep_pipelines_tests/resources/CEC/202008120411310459/clean/14_Hashiye_Ka_Siddhant_aur_Sahitya.wav',
-                              'ekstep_pipelines_tests/resources/CEC/202008120411310459/clean/12_Hashiye_Ka_Siddhant_aur_Sahitya.wav'],
+                             [('ekstep_pipelines_tests/resources/CEC/202008120411310459/clean/13_Hashiye_Ka_Siddhant_aur_Sahitya.wav', 0),
+                              ('ekstep_pipelines_tests/resources/CEC/202008120411310459/clean/14_Hashiye_Ka_Siddhant_aur_Sahitya.wav', 1),
+                              ('ekstep_pipelines_tests/resources/CEC/202008120411310459/clean/12_Hashiye_Ka_Siddhant_aur_Sahitya.wav', 0)],
                          'mkb_sp_1':
-                             ['ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/149_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',
-                              'ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/150_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',
-                              'ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/153_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav'],
+                             [('ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/149_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0),
+                              ('ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/150_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0),
+                              ('ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/153_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',1)],
                           'mkb_sp_2':
-                             ['ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/155_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',
-                              'ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/151_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav']
+                             [('ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/155_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0),
+                              ('ekstep_pipelines_tests/resources/CEC/202008120507305747/clean/151_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0)]
                          }
         expected = {'mkb_sp_0':
-                             ['13_Hashiye_Ka_Siddhant_aur_Sahitya.wav',
-                              '14_Hashiye_Ka_Siddhant_aur_Sahitya.wav',
-                              '12_Hashiye_Ka_Siddhant_aur_Sahitya.wav'],
+                             [('13_Hashiye_Ka_Siddhant_aur_Sahitya.wav', 0),
+                              ('14_Hashiye_Ka_Siddhant_aur_Sahitya.wav',1),
+                              ('12_Hashiye_Ka_Siddhant_aur_Sahitya.wav',0)],
                      'mkb_sp_1':
-                         ['149_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',
-                          '150_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',
-                          '153_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav'],
+                         [('149_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0),
+                          ('150_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0),
+                          ('153_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',1)],
                      'mkb_sp_2':
-                         ['155_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',
-                          '151_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav']
+                         [('155_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0),
+                          ('151_Hindi_Bhasha_ke_Vikas_ki_Purvpithika.wav',0)]
                      }
         speaker_map = speaker_to_file_name_map(file_map_dict)
         print(len(speaker_map))
