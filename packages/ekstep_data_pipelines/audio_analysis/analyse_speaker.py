1870fb1d5c3098dd9bb24fccffcd9e24e722847f
Heera ballabh <hballabh@onecallcm.com>
2021-01-13 11:34:44 +0530

    test(Audio_analysis): Add log for config file.

--- packages/ekstep_data_pipelines/audio_analysis/analyse_speaker.py
+++ packages/ekstep_data_pipelines/audio_analysis/analyse_speaker.py
@@ -15,7 +15,7 @@ def analyse_speakers(embed_file_path, dir_pattern, local_audio_download_path, so
     encoder(local_audio_download_path, dir_pattern, embed_file_path)
     is_uploaded = fs_interface.upload_to_location(embed_file_path, npz_bucket_destination_path)
     if is_uploaded:
-        Logger.info('npz file uploaded to :' + npz_bucket_destination_path)
+        Logger.info('npz file uploaded to gcp :' + npz_bucket_destination_path)
     else:
         Logger.info('npz file could not be uploaded to :' + npz_bucket_destination_path)
     file_map_dict, noise_file_map_dict = create_speaker_clusters(embed_file_path, source, min_cluster_size, partial_set_size, min_samples, fit_noise_on_similarity)
