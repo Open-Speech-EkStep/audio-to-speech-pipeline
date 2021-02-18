6db830ce5a449f0bb4e9e27051cc3eaa1f083d09
Heera ballabh <hballabh@onecallcm.com>
2021-02-18 19:37:53 +0530

    chore(readme): add MIT license for open source

--- packages/ekstep_data_pipelines/audio_analysis/analyse_gender.py
+++ packages/ekstep_data_pipelines/audio_analysis/analyse_gender.py
@@ -8,6 +8,7 @@ Logger = get_logger("analyse_speakers")
 
 
 def analyse_gender(embed_file_path):
+    Logger.info("Start analyse gender")
     gender_model = load_model('ekstep_data_pipelines/audio_analysis/models/clf_svc.sav')
     file_to_speaker_gender_mapping = get_prediction_from_npz_file(gender_model, embed_file_path)
     return file_to_speaker_gender_mapping
