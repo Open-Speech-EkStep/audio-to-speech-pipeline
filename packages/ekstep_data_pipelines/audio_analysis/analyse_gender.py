bff5feb7121b76d170372df79905c086fb9a1209
Rajat Singhal <rajasinghal@gmail.com>
2021-02-18 22:57:42 +0530

    chore(code_style): format files not compliant with black

--- packages/ekstep_data_pipelines/audio_analysis/analyse_gender.py
+++ packages/ekstep_data_pipelines/audio_analysis/analyse_gender.py
@@ -1,7 +1,9 @@
-
 from ekstep_data_pipelines.common.utils import get_logger
 
-from ekstep_data_pipelines.audio_analysis.audio_embeddings.gender_inference import load_model, get_prediction_from_npz_file
+from ekstep_data_pipelines.audio_analysis.audio_embeddings.gender_inference import (
+    load_model,
+    get_prediction_from_npz_file,
+)
 
 
 Logger = get_logger("analyse_speakers")
@@ -9,6 +11,8 @@ Logger = get_logger("analyse_speakers")
 
 def analyse_gender(embed_file_path):
     Logger.info("Start analyse gender")
-    gender_model = load_model('ekstep_data_pipelines/audio_analysis/models/clf_svc.sav')
-    file_to_speaker_gender_mapping = get_prediction_from_npz_file(gender_model, embed_file_path)
+    gender_model = load_model("ekstep_data_pipelines/audio_analysis/models/clf_svc.sav")
+    file_to_speaker_gender_mapping = get_prediction_from_npz_file(
+        gender_model, embed_file_path
+    )
     return file_to_speaker_gender_mapping
--- packages/ekstep_pipelines_tests/audio_analysis_test/speaker_analysis/audio_speaker_clustering_tests.py
+++ packages/ekstep_pipelines_tests/audio_analysis_test/speaker_analysis/audio_speaker_clustering_tests.py
@@ -1,28 +1,37 @@
 import sys
 import unittest
 
-from ekstep_data_pipelines.audio_analysis.speaker_analysis.create_embeddings import encoder
-from ekstep_data_pipelines.audio_analysis.speaker_analysis.speaker_clustering import create_speaker_clusters
+from ekstep_data_pipelines.audio_analysis.speaker_analysis.create_embeddings import (
+    encoder,
+)
+from ekstep_data_pipelines.audio_analysis.speaker_analysis.speaker_clustering import (
+    create_speaker_clusters,
+)
 from os import path
 
 # sys.path.insert(0, '..')
 
 
 class AudioSpeakerClusteringTests(unittest.TestCase):
-
     def test_should_create_embeddings_for_wav_files_set(self):
-        source_path = 'ekstep_pipelines_tests/resources/test_source/'
-        embed_file_name = '/tmp/embed_map.npz'
-        encoder(source_dir=source_path,
-                source_dir_pattern='*/clean/*.wav',
-                embed_file_name=embed_file_name)
+        source_path = "ekstep_pipelines_tests/resources/test_source/"
+        embed_file_name = "/tmp/embed_map.npz"
+        encoder(
+            source_dir=source_path,
+            source_dir_pattern="*/clean/*.wav",
+            embed_file_name=embed_file_name,
+        )
         self.assertTrue(path.exists(embed_file_name))
 
     def test_should_map_speakers_to_utterance_for_given_npz(self):
-        embed_file_name ='ekstep_pipelines_tests/resources/embed_map.npz'
+        embed_file_name = "ekstep_pipelines_tests/resources/embed_map.npz"
         file_map_dict, noise_file_map_dict = create_speaker_clusters(
             embed_filename_map_path=embed_file_name,
-            source_name='mkb', min_cluster_size=2, partial_set_size=200, min_samples=2)
+            source_name="mkb",
+            min_cluster_size=2,
+            partial_set_size=200,
+            min_samples=2,
+        )
         print(len(file_map_dict))
         print(file_map_dict)
         self.assertEqual(2, len(file_map_dict))
