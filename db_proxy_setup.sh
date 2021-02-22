aee9fe034d8c5a8e2744dd978108e1e6b970de46
Rajat Singhal <rajasinghal@gmail.com>
2021-02-22 19:39:05 +0530

    fix(build): fix env variable passing

--- db_proxy_setup.sh
+++ db_proxy_setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:Open-Speech-EkStep/E2ETest.git
-pwd
+REGION=us-central1
+DB_NAME=crowdsourcedb
+GOOGLE_AUTH=$1
+
 cd ${HOME}
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-265.0.0-linux-x86_64.tar.gz
 tar -zxf google-cloud-sdk-*
@@ -15,6 +17,6 @@ export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcp-key.json
 echo $GOOGLE_APPLICATION_CREDENTIALS
 wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
 chmod +x cloud_sql_proxy
-nohup ./cloud_sql_proxy -dir=./cloudsql -instances=ekstepspeechrecognition:us-central1:crowdsourcedb=tcp:5432 &
+nohup ./cloud_sql_proxy -dir=./cloudsql -instances=${GCP_PROJECT}:${REGION}:${DB_NAME}=tcp:5432 &
 sleep 25s
 cat nohup.out
