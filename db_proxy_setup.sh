94ae3d57056210e4f83685db82df60bcc0bc4610
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-02-22 19:40:32 +0530

    fix(pipeline):307| Soujyo | Test config.yaml

--- db_proxy_setup.sh
+++ db_proxy_setup.sh
@@ -3,7 +3,6 @@
 REGION=us-central1
 DB_NAME=crowdsourcedb
 GOOGLE_AUTH=$1
-
 cd ${HOME}
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-265.0.0-linux-x86_64.tar.gz
 tar -zxf google-cloud-sdk-*
