0b399edf726395b117544b0cf6f3d162e36a0f67
Rajat Singhal <rajasinghal@gmail.com>
2021-04-05 12:43:32 +0530

    chore(santitizer): add english language santizer

---
+++ packages/ekstep_data_pipelines/audio_transcription/transcription_sanitizers/indian_english_sanitizer.py
@@ -0,0 +1,62 @@
+from ekstep_data_pipelines.audio_transcription.transcription_sanitizers import (
+    BaseTranscriptionSanitizer,
+)
+from ekstep_data_pipelines.audio_transcription.transcription_sanitizers.audio_transcription_errors import (
+    TranscriptionSanitizationError,
+)
+
+from ekstep_data_pipelines.common.utils import get_logger
+import re
+
+LOGGER = get_logger("IndianEnglishSanitizer")
+
+
+class IndianEnglishSanitizer(BaseTranscriptionSanitizer):
+
+    VALID_CHARS = ""
+    PUNCTUATION = ""
+
+    @staticmethod
+    def get_instance(**kwargs):
+        return IndianEnglishSanitizer()
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def sanitize(self, transcription):
+        LOGGER.info("Sanitizing transcription:" + transcription)
+        transcription = transcription.strip()
+
+        transcription = self.replace_bad_char(transcription)
+
+        transcription = transcription.strip()
+
+        if len(transcription) == 0:
+            raise TranscriptionSanitizationError("transcription is empty")
+
+        if self.shouldReject(transcription):
+            raise TranscriptionSanitizationError(
+                "transcription has char which is not in  "
+            )
+
+        return transcription
+
+    def shouldReject(self, transcription):
+        rejected_string = re.sub(
+            pattern=IndianEnglishSanitizer.VALID_CHARS, repl="", string=transcription
+        )
+
+        if len(rejected_string.strip()) > 0:
+            return True
+
+        return False
+
+    def replace_bad_char(self, transcription):
+
+        LOGGER.info("replace panctuation if present ")
+
+        if "-" in transcription:
+            transcription = transcription.replace("-", " ")
+
+        table = str.maketrans(dict.fromkeys(IndianEnglishSanitizer.PUNCTUATION))
+        return transcription.translate(table)
--- packages/ekstep_data_pipelines/audio_transcription/transcription_sanitizers/utils.py
+++ packages/ekstep_data_pipelines/audio_transcription/transcription_sanitizers/utils.py
@@ -2,10 +2,12 @@ def get_transcription_sanitizers(**kwargs):
     hindi_sanitizer = HindiSanitizer.get_instance(**kwargs)
     gujrati_sanitizer = GujratiSanitizer.get_instance(**kwargs)
     kannada_sanitizer = KannadaSanitizer.get_instance(**kwargs)
+    indian_english_sanitizer = IndianEnglishSanitizer.get_instance(**kwargs)
 
     return {
         "hindi_sanitizer": hindi_sanitizer,
         "gujrati": gujrati_sanitizer,
         "default": hindi_sanitizer,
         "kannada": kannada_sanitizer,
+        "indian_english": indian_english_sanitizer,
     }
