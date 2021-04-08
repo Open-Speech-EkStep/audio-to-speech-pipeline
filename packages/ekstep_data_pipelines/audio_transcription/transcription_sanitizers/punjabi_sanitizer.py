0baefaeb74e67c12b3dd0737dd3130e51c889c84
Rishabh-0412 <rishabh.gaur@thoughtworks.com>
2021-04-08 15:11:37 +0530

    chore(punjabi): Added santization rules for punjabi

---
+++ packages/ekstep_data_pipelines/audio_transcription/transcription_sanitizers/punjabi_sanitizer.py
@@ -0,0 +1,62 @@
+from ekstep_data_pipelines.audio_transcription.transcription_sanitizers import (
+    BaseTranscriptionSanitizer,
+)
+from ekstep_data_pipelines.audio_transcription.transcription_sanitizers.audio_transcription_errors import (
+    TranscriptionSanitizationError,
+)
+
+from ekstep_data_pipelines.common.utils import get_logger
+import re
+
+LOGGER = get_logger("PunjabiSanitizer")
+
+
+class PunjabiSanitizer(BaseTranscriptionSanitizer):
+
+    VALID_CHARS = "[  ੍ੑ ਾ ਿ ੀ ੁ ੂ ੇ ੈ ੋੰੱਅ-ਊਏ-ਐਓ-ਨਪ-ਰਲਲ਼ਵਸ਼ਖ਼-ੜਫ਼]+"
+    PUNCTUATION = "!\"#%&'()*+,./;<=>?@[\\]^_`{|}~।"
+
+    @staticmethod
+    def get_instance(**kwargs):
+        return PunjabiSanitizer()
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def sanitize(self, transcription):
+        LOGGER.info("Sanitizing transcription:" + transcription)
+        transcription = transcription.strip()
+
+        transcription = self.replace_bad_char(transcription)
+
+        transcription = transcription.strip()
+
+        if len(transcription) == 0:
+            raise TranscriptionSanitizationError("transcription is empty")
+
+        if self.shouldReject(transcription):
+            raise TranscriptionSanitizationError(
+                "transcription has char which is not in ੍ੑ ਾ ਿ ੀ ੁ ੂ ੇ ੈ ੋੰੱਅ-ਊਏ-ਐਓ-ਨਪ-ਰਲਲ਼ਵਸ਼ਖ਼-ੜਫ਼"
+            )
+
+        return transcription
+
+    def shouldReject(self, transcription):
+        rejected_string = re.sub(
+            pattern=PunjabiSanitizer.VALID_CHARS, repl="", string=transcription
+        )
+
+        if len(rejected_string.strip()) > 0:
+            return True
+
+        return False
+
+    def replace_bad_char(self, transcription):
+
+        LOGGER.info("replace panctuation if present ")
+
+        if "-" in transcription:
+            transcription = transcription.replace("-", " ")
+
+        table = str.maketrans(dict.fromkeys(PunjabiSanitizer.PUNCTUATION))
+        return transcription.translate(table)
--- packages/ekstep_data_pipelines/audio_transcription/transcription_sanitizers/utils.py
+++ packages/ekstep_data_pipelines/audio_transcription/transcription_sanitizers/utils.py
@@ -3,11 +3,13 @@ def get_transcription_sanitizers(**kwargs):
     gujrati_sanitizer = GujratiSanitizer.get_instance(**kwargs)
     kannada_sanitizer = KannadaSanitizer.get_instance(**kwargs)
     indian_english_sanitizer = IndianEnglishSanitizer.get_instance(**kwargs)
-
+    punjabi_sanitizer = PunjabiSanitizer.get_instance(**kwargs)
+    
     return {
         "hindi_sanitizer": hindi_sanitizer,
         "gujrati": gujrati_sanitizer,
         "default": hindi_sanitizer,
         "kannada": kannada_sanitizer,
-        "indian_english": indian_english_sanitizer
+        "indian_english": indian_english_sanitizer,
+        "punjabi": punjabi_sanitizer
     }
