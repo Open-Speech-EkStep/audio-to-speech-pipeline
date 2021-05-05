b08f18ba05d4956991ca782f650bf73173653e77
soujyosen@gmail.com <soujyo.sen@thoughtworks.com>
2021-05-06 02:04:52 +0530

    718 | Soujyo | perf(data_filtering): Add csv file support for data filtering

--- packages/ekstep_data_pipelines/common/file_utils.py
+++ packages/ekstep_data_pipelines/common/file_utils.py
@@ -1,2 +1,9 @@
+import os
 def get_file_name(file_path):
     return file_path.split("/").pop()
+
+def check_file_exits(file_path):
+    return os.path.isfile(file_path)
+
+def ensure_path(path):
+    os.makedirs(path, exist_ok=True)
--- packages/ekstep_data_pipelines/data_marker/constants.py
+++ packages/ekstep_data_pipelines/data_marker/constants.py
@@ -2,3 +2,6 @@ CONFIG_NAME = "data_marker_config"
 FILTER_CRITERIA = "filter"
 LANDING_BASE_PATH = "landing_directory_path"
 SOURCE_BASE_PATH = "source_directory_path"
+FILE_MODE = "file_mode"
+FILE_PATH = "file_path"
+FILE_COLUMN_LIST = ["speaker_id", "clipped_utterance_file_name", "clipped_utterance_duration", "audio_id", "snr"]
