ac9f9e649bd8c74e8f1cbe30a70989d403c32128
Rajat Singhal <rajasinghal@gmail.com>
2021-06-06 16:32:55 +0530

    fix(ulca): add gender in query to get utterances

--- packages/ekstep_pipelines_tests/catalogue_dao_tests.py
+++ packages/ekstep_pipelines_tests/catalogue_dao_tests.py
@@ -308,7 +308,7 @@ class CatalogueTests(unittest.TestCase):
             """
             select msp.clipped_utterance_file_name as audio_file_name, 
             msp.clipped_utterance_duration as duration, msp.snr , s.speaker_name, 
-            mms.source_url as collection_source , mms.source_website as main_source, msp.gender as gender
+            mms.source_url as collection_source , mms.source_website as main_source, msp.speaker_gender as gender
             from media_speaker_mapping msp 
                 inner join media_metadata_staging mms 
                     on msp.audio_id = mms.audio_id
